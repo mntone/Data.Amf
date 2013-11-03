6057332665cc4827c7ad464eeb3b7dc8c5ff917d
mntone <sd8@live.jp>
2013-11-03 22:16:21 +0900

    Add test.

--- Mntone.Data.Amf.UnitTest/Amf3UnitTest.cpp
+++ Mntone.Data.Amf.UnitTest/Amf3UnitTest.cpp
@@ -125,101 +125,106 @@ public:
 		} );
 	}
 
-
-	TEST_METHOD( FromValueParseTest_Double0 )
+	TEST_METHOD( Amf3_⑤DoubleTest‐0_0．0 )
 	{
-		ParseTestDouble0( ValueParse );
+		TestAmf3( ref new TestByteArray{ 5, 0, 0, 0, 0, 0, 0, 0, 0 }, []( IAmfValue^ amfValue )
+		{
+			Assert::IsTrue( amfValue->ValueType == AmfValueType::Double );
+			Assert::AreEqual( 0.0, amfValue->GetDouble() );
+		} );
 	}
 
-	TEST_METHOD( FromValueParseTest_ObjectNull )
+	TEST_METHOD( Amf3_⑤DoubleTest‐1_0．5 )
 	{
-		ParseTestObjectNull( ValueParse );
+		TestAmf3( ref new TestByteArray{ 5, 0x3f, 0xe0, 0, 0, 0, 0, 0, 0 }, []( IAmfValue^ amfValue )
+		{
+			Assert::IsTrue( amfValue->ValueType == AmfValueType::Double );
+			Assert::AreEqual( 0.5, amfValue->GetDouble() );
+		} );
 	}
 
-	TEST_METHOD( FromValueParseTest_ArrayNull )
+	TEST_METHOD( Amf3_⑥StringTest‐0_Null )
 	{
-		ParseTestArrayNull( ValueParse );
+		TestAmf3( ref new TestByteArray{ 6, 1 }, []( IAmfValue^ amfValue )
+		{
+			Assert::IsTrue( amfValue->ValueType == AmfValueType::String );
+			Assert::AreEqual( amfValue->GetString()->Data(), L"" );
+		} );
 	}
 
-	TEST_METHOD( FromObjectParseTest_Double0 )
+	TEST_METHOD( Amf3_⑥StringTest‐1_Ascii )
 	{
-		ParseTestDouble0( ObjectParse );
+		TestAmf3( ref new TestByteArray{ 6, 11, 0x41, 0x53, 0x43, 0x49, 0x49 }, []( IAmfValue^ amfValue )
+		{
+			Assert::IsTrue( amfValue->ValueType == AmfValueType::String );
+			Assert::AreEqual( amfValue->GetString()->Data(), L"ASCII" );
+		} );
 	}
 
-	TEST_METHOD( FromObjectParseTest_ObjectNull )
+	TEST_METHOD( Amf3_⑥StringTest‐2_RandomCharactors1 )
 	{
-		ParseTestObjectNull( ObjectParse );
+		TestAmf3( ref new TestByteArray{ 6, 13, 0xc2, 0xbd, 0x3a, 0x20, 0x32, 0x42 }, []( IAmfValue^ amfValue )
+		{
+			Assert::IsTrue( amfValue->ValueType == AmfValueType::String );
+			Assert::AreEqual( L"½: 2B", amfValue->GetString()->Data() );
+		} );
 	}
 
-	TEST_METHOD( FromObjectParseTest_ArrayNull )
+	TEST_METHOD( Amf3_⑥StringTest‐3_RandomCharactors2 )
 	{
-		ParseTestArrayNull( ObjectParse );
+		TestAmf3( ref new TestByteArray{ 6, 15, 0xe2, 0x91, 0xb4, 0x3a, 0x20, 0x33, 0x42 }, []( IAmfValue^ amfValue )
+		{
+			Assert::IsTrue( amfValue->ValueType == AmfValueType::String );
+			Assert::AreEqual( L"⑴: 3B", amfValue->GetString()->Data() );
+		} );
 	}
 
-	TEST_METHOD( FromArrayParseTest_Double0 )
+	TEST_METHOD( Amf3_⑦XmlDocumentTest‐0_RootNode )
 	{
-		ParseTestDouble0( ArrayParse );
+		TestAmf3( ref new TestByteArray{ 7, 13, 0x3c, 0x72, 0x6f, 0x6f, 0x74, 0x3e }, []( IAmfValue^ amfValue )
+		{
+			Assert::IsTrue( amfValue->ValueType == AmfValueType::Xml );
+			Assert::AreEqual( L"<root>", amfValue->GetString()->Data() );
+		} );
 	}
 
-	TEST_METHOD( FromArrayParseTest_ObjectNull )
+	TEST_METHOD( Amf3_⑧DateTest‐0_2013_10_13 )
 	{
-		ParseTestObjectNull( ArrayParse );
+		TestAmf3( ref new TestByteArray{ 8, 1, 0x42, 0x74, 0x1a, 0xd2, 0xe6, 0x18, 0x00, 0x00 }, []( IAmfValue^ amfValue )
+		{
+			Assert::IsTrue( amfValue->ValueType == AmfValueType::Date );
+
+			const auto& dateFormat = ref new WG::DateTimeFormatting::DateTimeFormatter(
+				WG::DateTimeFormatting::YearFormat::Full,
+				WG::DateTimeFormatting::MonthFormat::Numeric,
+				WG::DateTimeFormatting::DayFormat::Default,
+				WG::DateTimeFormatting::DayOfWeekFormat::None );
+			Assert::AreEqual( L"‎2013‎年‎10‎月‎13‎日", dateFormat->Format( amfValue->GetDate() )->Data() );
+		} );
 	}
 
-	TEST_METHOD( FromArrayParseTest_ArrayNull )
+	TEST_METHOD( Amf3_⑧DateTest‐1_2013_11_02_20_28_52_0100 )
 	{
-		ParseTestArrayNull( ArrayParse );
-	}
+		TestAmf3( ref new TestByteArray{ 8, 1, 0x42, 0x74, 0x21, 0x89, 0x2a, 0x18, 0x40, 0 }, []( IAmfValue^ amfValue )
+		{
+			Assert::IsTrue( amfValue->ValueType == AmfValueType::Date );
 
-	TEST_METHOD( FromValueTryParseTest_Double0 )
-	{
-		ParseTestDouble0( ValueTryParse );
+			const auto& dateFormat = ref new WG::DateTimeFormatting::DateTimeFormatter(
+				WG::DateTimeFormatting::YearFormat::Full,
+				WG::DateTimeFormatting::MonthFormat::Numeric,
+				WG::DateTimeFormatting::DayFormat::Default,
+				WG::DateTimeFormatting::DayOfWeekFormat::None,
+				WG::DateTimeFormatting::HourFormat::Default,
+				WG::DateTimeFormatting::MinuteFormat::Default,
+				WG::DateTimeFormatting::SecondFormat::Default,
+				ref new Platform::Collections::Vector<Platform::String^>( { "ja" } ) );
+			Assert::AreEqual( L"2013年11月2日 20:28:52", dateFormat->Format( amfValue->GetDate() )->Data() );
+		} );
 	}
-
-	TEST_METHOD( FromValueTryParseTest_ObjectNull )
-	{
-		ParseTestObjectNull( ValueTryParse );
-	}
-
-	TEST_METHOD( FromValueTryParseTest_ArrayNull )
-	{
-		ParseTestArrayNull( ValueTryParse );
-	}
-
-	TEST_METHOD( FromObjectTryParseTest_Double0 )
-	{
-		ParseTestDouble0( ObjectTryParse );
-	}
-
-	TEST_METHOD( FromObjectTryParseTest_ObjectNull )
-	{
-		ParseTestObjectNull( ObjectTryParse );
-	}
-
-	TEST_METHOD( FromObjectTryParseTest_ArrayNull )
-	{
-		ParseTestArrayNull( ObjectTryParse );
-	}
-
-	TEST_METHOD( FromArrayTryParseTest_Double0 )
-	{
-		ParseTestDouble0( ArrayTryParse );
-	}
-
-	TEST_METHOD( FromArrayTryParseTest_ObjectNull )
-	{
-		ParseTestObjectNull( ArrayTryParse );
-	}
-
-	TEST_METHOD( FromArrayTryParseTest_ArrayNull )
-	{
-		ParseTestArrayNull( ArrayTryParse );
-	}
-
 
 	TEST_METHOD( Amf3_MasterTest_RtmpCommandData )
 	{
-		TestAmf3( ref new TestByteArray{ 10, 0, 0, 0, 4, 0x02, 0x00, 0x07, 0x5f, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x00, 0x3f, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x06, 0x66, 0x6d, 0x73, 0x56, 0x65, 0x72, 0x02, 0x00, 0x0d, 0x46, 0x4d, 0x53, 0x2f, 0x33, 0x2c, 0x30, 0x2c, 0x31, 0x2c, 0x31, 0x32, 0x33, 0x00, 0x0c, 0x63, 0x61, 0x70, 0x61, 0x62, 0x69, 0x6c, 0x69, 0x74, 0x69, 0x65, 0x73, 0x00, 0x40, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x03, 0x00, 0x05, 0x6c, 0x65, 0x76, 0x65, 0x6c, 0x02, 0x00, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x00, 0x04, 0x63, 0x6f, 0x64, 0x65, 0x02, 0x00, 0x1d, 0x4e, 0x65, 0x74, 0x43, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x43, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74, 0x2e, 0x53, 0x75, 0x63, 0x63, 0x65, 0x73, 0x73, 0x00, 0x0b, 0x64, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x02, 0x00, 0x14, 0x43, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x20, 0x73, 0x75, 0x63, 0x63, 0x65, 0x65, 0x64, 0x65, 0x64, 0x00, 0x0e, 0x6f, 0x62, 0x6a, 0x65, 0x63, 0x74, 0x45, 0x6e, 0x63, 0x6f, 0x64, 0x69, 0x6e, 0x67, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09 }, []( IAmfValue^ amfValue )
+		TestAmf3( ref new TestByteArray{ 0x9, 0x9, 0x1, 0x6, 0xf, 0x5f, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x4, 0x1, 0xa, 0xb, 0x1, 0x19, 0x63, 0x61, 0x70, 0x61, 0x62, 0x69, 0x6c, 0x69, 0x74, 0x69, 0x65, 0x73, 0x4, 0x1f, 0xd, 0x66, 0x6d, 0x73, 0x56, 0x65, 0x72, 0x6, 0x1b, 0x46, 0x4d, 0x53, 0x2f, 0x33, 0x2c, 0x30, 0x2c, 0x31, 0x2c, 0x31, 0x32, 0x33, 0x1, 0xa, 0x1, 0x17, 0x64, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x6, 0x29, 0x43, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x20, 0x73, 0x75, 0x63, 0x63, 0x65, 0x65, 0x64, 0x65, 0x64, 0x1d, 0x6f, 0x62, 0x6a, 0x65, 0x63, 0x74, 0x45, 0x6e, 0x63, 0x6f, 0x64, 0x69, 0x6e, 0x67, 0x4, 0x0, 0x9, 0x63, 0x6f, 0x64, 0x65, 0x6, 0x3b, 0x4e, 0x65, 0x74, 0x43, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x43, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74, 0x2e, 0x53, 0x75, 0x63, 0x63, 0x65, 0x73, 0x73, 0xb, 0x6c, 0x65, 0x76, 0x65, 0x6c, 0x6, 0xd, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x1 }, []( IAmfValue^ amfValue )
 		{
 			Assert::IsTrue( amfValue->ValueType == AmfValueType::Array );
 
@@ -240,7 +245,6 @@ public:
 
 			const auto& obj2 = objLapped2->GetObject();
 			Assert::AreEqual( L"status", obj2->GetNamedString( "level" )->Data() );
-
 			Assert::AreEqual( L"NetConnection.Connect.Success", obj2->GetNamedString( "code" )->Data() );
 			Assert::AreEqual( L"Connection succeeded", obj2->GetNamedString( "description" )->Data() );
 			Assert::AreEqual( 0.0, obj2->GetNamedDouble( "objectEncoding" ) );
@@ -248,78 +252,24 @@ public:
 	}
 
 private:
-	static void ParseTestDouble0( std::function<IAmfValue^ ( TestByteArray^ )> parseFunc )
-	{
-		Assert::AreEqual( 0.0, parseFunc( ref new TestByteArray{ 0, 0, 0, 0, 0, 0, 0, 0, 0 } )->GetDouble() );
-	}
-
-	static void ParseTestObjectNull( std::function<IAmfValue^ ( TestByteArray^ )> parseFunc )
-	{
-		Assert::AreEqual<uint32>( 0, parseFunc( ref new TestByteArray{ 3, 0, 0, 9 } )->GetObject()->Size );
-	}
-
-	static void ParseTestArrayNull( std::function<IAmfValue^ ( TestByteArray^ )> parseFunc )
-	{
-		Assert::AreEqual<uint32>( 0, parseFunc( ref new TestByteArray{ 10, 0, 0, 0, 0 } )->GetArray()->Size );
-	}
-
-	static AmfValue^ ValueParse( TestByteArray^ byteArray )
-	{
-		return AmfValue::Parse( byteArray, AmfEncodingType::Amf0 );
-	}
-
-	static AmfValue^ ValueTryParse( TestByteArray^ byteArray )
-	{
-		AmfValue^ outValue;
-		AmfValue::TryParse( byteArray, AmfEncodingType::Amf0, &outValue );
-		return outValue;
-	}
-
-	static AmfObject^ ObjectParse( TestByteArray^ byteArray )
-	{
-		return AmfObject::Parse( byteArray, AmfEncodingType::Amf0 );
-	}
-
-	static AmfObject^ ObjectTryParse( TestByteArray^ byteArray )
-	{
-		AmfObject^ outObject;
-		AmfObject::TryParse( byteArray, AmfEncodingType::Amf0, &outObject );
-		return outObject;
-	}
-
-	static AmfArray^ ArrayParse( TestByteArray^ byteArray )
-	{
-		return AmfArray::Parse( byteArray, AmfEncodingType::Amf0 );
-	}
-
-	static AmfArray^ ArrayTryParse( TestByteArray^ byteArray )
-	{
-		AmfArray^ outArray;
-		AmfArray::TryParse( byteArray, AmfEncodingType::Amf0, &outArray );
-		return outArray;
-	}
-
-
 	static void TestAmf3( TestByteArray^ rawData, std::function<void( IAmfValue^ )> checkHandler )
 	{
-		auto r = Amf3Parser::Parse( rawData );
-		auto st = r->ToString();
-		checkHandler( r );
-		//const auto& create_data = Amf0Sequencer::Sequenceify( r );
+		const auto& amf = Amf3Parser::Parse( rawData );
+		const auto& str = amf->ToString();
+		Logger::WriteMessage( str->Data() );
+		checkHandler( amf );
 
-		//Assert::IsTrue( ArrayEquals( rawData, create_data ) );
+		//const auto& createData = Amf0Sequencer::Sequenceify( r );
+		//Assert::AreEqual( rawData->Length, createData->Length );
+		//Assert::IsTrue( ArrayEquals( rawData, createData ) );
 	}
 
 	static bool ArrayEquals( const Platform::Array<uint8>^ firstArray, const Platform::Array<uint8>^ secondArray )
 	{
-		if( firstArray->Length != secondArray->Length )
-			return false;
 		for( auto i = 0u; i < firstArray->Length; ++i )
 		{
 			if( !secondArray[i].Equals( secondArray[i] ) )
-			{
 				return false;
-			}
 		}
 		return true;
 	}
