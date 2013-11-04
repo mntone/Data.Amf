4cb50c65610ce8f0222b4ab83f6bd69ca9476214
クワーティ <riot313@gmail.com>
2013-11-04 22:05:26 +0900

    add AmfByteArray test cases

---
+++ Mntone.Data.Amf.UnitTest/AmfByteArrayValueUnitTest.cpp
@@ -0,0 +1,125 @@
+
+
+#include "pch.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Mntone::Data::Amf;
+
+
+TEST_CLASS(AmfByteArrayValueUnitTest)
+{
+	
+
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_CreateTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::IsTrue(AmfValueType::ByteArray == amfValue->ValueType);
+	}
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetBooleanTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetDoubleTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetIntegerTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+
+	}
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetStringTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetDateTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetByteArrayTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+		auto testArray = CreateTestArray();
+		AssertHelper::AreArrayEqual(testArray, amfValue->GetByteArray());
+	}
+
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetVectorIntTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetVectorUintTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetVectorDoubleTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetObjectTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_GetArrayTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfByteArrayValueUnitTest_ToStringTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+		auto testArray = CreateTestArray();
+		Assert::AreEqual(testArray->ToString(), amfValue->ToString());
+	}
+
+private:
+	AmfValue^ GeneralCareteAmfValue(){
+
+		return AmfValue::CreateByteArrayValue(CreateTestArray());
+	}
+
+	
+	private :Platform::Array<uint8>^ CreateTestArray(){
+
+		return ref new Platform::Array<uint8>{5,4,32,22};
+	}
+
+
+
+
+};
