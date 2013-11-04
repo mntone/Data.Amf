8d5cc051d1ee63a5bc75664023a611d1bc9a18b2
クワーティ <riot313@gmail.com>
2013-11-04 23:14:03 +0900

    add AmfVectorUintValue test cases

---
+++ Mntone.Data.Amf.UnitTest/AmfVectorUintValueUnitTest.cpp
@@ -0,0 +1,121 @@
+
+
+#include "pch.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Mntone::Data::Amf;
+
+
+TEST_CLASS(AmfVectorUintValueUnitTest)
+{
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_CreateTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::IsTrue(AmfValueType::VectorUint == amfValue->ValueType);
+	}
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetBooleanTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetDoubleTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetIntegerTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+
+	}
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetStringTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetDateTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetByteArrayTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetVectorIntTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetVectorUintTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		AssertHelper::AreVectorEqual(generalTestVector, amfValue->GetVectorUint());
+	}
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetVectorDoubleTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetObjectTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_GetArrayTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfVectorUintValueUnitTest_ToStringTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::AreEqual(generalTestVector->ToString(), amfValue->ToString());
+	}
+
+private:
+	AmfValue^ GeneralCareteAmfValue(){
+
+		return AmfValue::CreateVectorUintValue(generalTestVector);
+	}
+
+	Windows::Foundation::Collections::IVector<uint32>^ CreateGeneralTestVector(){
+
+		return ref new Platform::Collections::Vector<uint32>{ 47, 44, 22 };
+	}
+
+	Windows::Foundation::Collections::IVector<uint32>^ generalTestVector = CreateGeneralTestVector();
+
+};
