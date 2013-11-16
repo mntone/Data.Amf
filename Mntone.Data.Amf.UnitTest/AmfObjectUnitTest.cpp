d7e2586fedff3c8cac117b934e0c8c4d2e3fdc5f
クワーティ <riot313@gmail.com>
2013-11-16 23:17:57 +0900

    add AmfObjectUnitTest

---
+++ Mntone.Data.Amf.UnitTest/AmfObjectUnitTest.cpp
@@ -0,0 +1,121 @@
+#include "pch.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Mntone::Data::Amf;
+
+
+TEST_CLASS(AmfObjectUnitTest)
+{
+
+	TEST_METHOD(AmfObject_CreateTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfObject_GetBooleanTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfObject_GetDoubleTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfObject_GetIntegerTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+
+	}
+
+	TEST_METHOD(AmfObject_GetStringTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfObject_GetDateTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfObject_GetByteArrayTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfObject_GetVectorIntTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfObject_GetVectorUintTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfObject_GetVectorDoubleTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfObject_GetVectorObjectTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfObject_GetObjectTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+
+	TEST_METHOD(AmfObject_GetArrayTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+	TEST_METHOD(AmfObject_ToStringTest){
+
+		auto amfValue = GeneralCareteAmfValue();
+
+		Assert::Fail(L"must write test");
+	}
+
+private:
+	AmfValue^ GeneralCareteAmfValue(){
+
+		//
+		//TODO:write create instance
+		//
+	}
+
+};
