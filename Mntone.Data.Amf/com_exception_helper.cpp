1229665b5041fa17d5d468d294fc9534dbc986e1
mntone <sd8@live.jp>
2014-02-18 18:17:12 +0900

    Update exception factory.

--- Mntone.Data.Amf/com_exception_helper.cpp
+++ Mntone.Data.Amf/com_exception_helper.cpp
@@ -1,10 +1,10 @@
 #include "pch.h"
 #include "com_exception_helper.h"
-#include "corerror.h"
+#include <corerror.h>
 
 using namespace mntone::data::amf;
 		
-Platform::COMException^	com_exception_helper::create_invalid_operation_exception( Platform::String^ message )
+Platform::Exception^ com_exception_helper::create_invalid_operation_exception( Platform::String^ message )
 {
-	return ref new Platform::COMException( COR_E_INVALIDOPERATION, message );
+	return Platform::Exception::CreateException( COR_E_INVALIDOPERATION, message );
 }
--- Mntone.Data.Amf/com_exception_helper.h
+++ Mntone.Data.Amf/com_exception_helper.h
@@ -5,7 +5,7 @@ namespace mntone { namespace data { namespace amf {
 	class com_exception_helper
 	{
 	public:
-		static Platform::COMException^ create_invalid_operation_exception( Platform::String^ message );
+		static Platform::Exception^ create_invalid_operation_exception( Platform::String^ message );
 	};
 
 } } }
